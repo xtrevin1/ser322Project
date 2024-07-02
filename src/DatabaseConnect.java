e4ad766a6cb7e7e8265996c4e68142ace6536569
Zeb <moffat.zeb@gmail.com>
2024-07-01 21:28:56 -0700

    Main class with menus and DB class able to print all tables

---
+++ src/DatabaseConnect.java
@@ -0,0 +1,475 @@
+import java.sql.*;
+
+public class DatabaseConnect {
+	
+	/**User entered arguments*/
+	private String url;
+	private String user;
+	private String password;
+	private String driver;
+	
+	/**mySQL related variables*/
+	private static Connection connection;
+	private static ResultSet resultSet;
+	private static Statement statement;
+	private static PreparedStatement preparedStatement;
+	
+	
+	public DatabaseConnect(String[] args) {
+		url = args[0];
+		user = args[1];
+		password = args[2];
+		driver = args[3];
+
+		System.out.println("Connecting to database through url: " + url + "\n");
+		
+	}
+	
+	public void printAlbumTable() {
+		try {
+			Class.forName(driver);
+			
+			connection = DriverManager.getConnection(url, user, password);
+			
+			statement = connection.createStatement();
+			
+			resultSet = statement.executeQuery(
+					"SELECT * FROM ALBUM");
+			
+			System.out.println("ALBUM\n");
+			System.out.println("AlbumID |         Name         | LabelID | ProducerID");
+			System.out.println("-----------------------------------------------------");
+			while (resultSet.next()) {
+				printSpaces(7 - String.valueOf(resultSet.getInt(1)).length());
+				System.out.print(resultSet.getInt(1) + " | ");
+				
+				printSpaces(20 - resultSet.getString(2).length());
+				System.out.print(resultSet.getString(2) + " | ");
+				
+				printSpaces(7 - String.valueOf(resultSet.getInt(3)).length());
+				System.out.print(resultSet.getInt(3) + " | ");
+				
+				printSpaces(10 - String.valueOf(resultSet.getInt(4)).length());
+				System.out.println(resultSet.getInt(4));
+			}
+			System.out.println();
+			
+		} catch (ClassNotFoundException e) {
+	        System.out.println("Error: JDBC driver class '" + driver + "' not found.");
+	        e.printStackTrace();
+		} catch (SQLException e) {
+	        System.out.println("SQL Error: " + e.getMessage());
+		} finally {
+			try {
+	            if (resultSet != null) {
+	            		resultSet.close();
+	            }
+	            if (statement != null) {
+	            	statement.close();
+	            }
+	            if (connection != null) {
+	            	connection.close();
+	            }
+	        } catch (SQLException e) {
+	            System.out.println("Error closing database resources: " + e.getMessage());
+	        }
+		}	
+	}
+	
+	public void printArtistTable() {
+		try {
+			Class.forName(driver);
+			
+			connection = DriverManager.getConnection(url, user, password);
+			
+			statement = connection.createStatement();
+			
+			resultSet = statement.executeQuery(
+					"SELECT * FROM ARTIST");
+			
+			System.out.println("ARTIST\n");
+			System.out.println("ArtistID |         Name         | LabelID");
+			System.out.println("-----------------------------------------");
+			while (resultSet.next()) {
+				printSpaces(8 - String.valueOf(resultSet.getInt(1)).length());
+				System.out.print(resultSet.getInt(1) + " | ");
+				
+				printSpaces(20 - resultSet.getString(2).length());
+				System.out.print(resultSet.getString(2) + " | ");
+				
+				printSpaces(7 - String.valueOf(resultSet.getInt(3)).length());
+				System.out.println(resultSet.getInt(3));
+			}
+			System.out.println();
+			
+		} catch (ClassNotFoundException e) {
+	        System.out.println("Error: JDBC driver class '" + driver + "' not found.");
+	        e.printStackTrace();
+		} catch (SQLException e) {
+	        System.out.println("SQL Error: " + e.getMessage());
+		} finally {
+			try {
+	            if (resultSet != null) {
+	            		resultSet.close();
+	            }
+	            if (statement != null) {
+	            	statement.close();
+	            }
+	            if (connection != null) {
+	            	connection.close();
+	            }
+	        } catch (SQLException e) {
+	            System.out.println("Error closing database resources: " + e.getMessage());
+	        }
+		}
+	}
+	
+	public void printCreatedTable() {
+		try {
+			Class.forName(driver);
+			
+			connection = DriverManager.getConnection(url, user, password);
+			
+			statement = connection.createStatement();
+			
+			resultSet = statement.executeQuery(
+					"SELECT * FROM CREATED");
+			
+			System.out.println("CREATED\n");
+			System.out.println("ArtistID | SongID");
+			System.out.println("-----------------");
+			while (resultSet.next()) {
+				printSpaces(8- String.valueOf(resultSet.getInt(1)).length());
+				System.out.print(resultSet.getInt(1) + " | ");
+								
+				printSpaces(6 - String.valueOf(resultSet.getInt(2)).length());
+				System.out.println(resultSet.getInt(2));
+			}
+			System.out.println();
+			
+		} catch (ClassNotFoundException e) {
+	        System.out.println("Error: JDBC driver class '" + driver + "' not found.");
+	        e.printStackTrace();
+		} catch (SQLException e) {
+	        System.out.println("SQL Error: " + e.getMessage());
+		} finally {
+			try {
+	            if (resultSet != null) {
+	            		resultSet.close();
+	            }
+	            if (statement != null) {
+	            	statement.close();
+	            }
+	            if (connection != null) {
+	            	connection.close();
+	            }
+	        } catch (SQLException e) {
+	            System.out.println("Error closing database resources: " + e.getMessage());
+	        }
+		}	
+	}
+	
+	public void printGenreTable() {
+		try {
+			Class.forName(driver);
+			
+			connection = DriverManager.getConnection(url, user, password);
+			
+			statement = connection.createStatement();
+			
+			resultSet = statement.executeQuery(
+					"SELECT * FROM GENRE");
+			
+			System.out.println("GENRE\n");
+			System.out.println("GenreID |         Name         ");
+			System.out.println("-------------------------------");
+			while (resultSet.next()) {
+				printSpaces(7 - String.valueOf(resultSet.getInt(1)).length());
+				System.out.print(resultSet.getInt(1) + " | ");
+				
+				printSpaces(21 - resultSet.getString(2).length());
+				System.out.println(resultSet.getString(2));
+			}
+			System.out.println();
+			
+		} catch (ClassNotFoundException e) {
+	        System.out.println("Error: JDBC driver class '" + driver + "' not found.");
+	        e.printStackTrace();
+		} catch (SQLException e) {
+	        System.out.println("SQL Error: " + e.getMessage());
+		} finally {
+			try {
+	            if (resultSet != null) {
+	            		resultSet.close();
+	            }
+	            if (statement != null) {
+	            	statement.close();
+	            }
+	            if (connection != null) {
+	            	connection.close();
+	            }
+	        } catch (SQLException e) {
+	            System.out.println("Error closing database resources: " + e.getMessage());
+	        }
+		}
+	}
+
+	public void printMadeTable() {
+		try {
+			Class.forName(driver);
+			
+			connection = DriverManager.getConnection(url, user, password);
+			
+			statement = connection.createStatement();
+			
+			resultSet = statement.executeQuery(
+					"SELECT * FROM MADE");
+			
+			System.out.println("MADE\n");
+			System.out.println("ArtistID | AlbumID");
+			System.out.println("------------------");
+			while (resultSet.next()) {
+				printSpaces(8 - String.valueOf(resultSet.getInt(1)).length());
+				System.out.print(resultSet.getInt(1) + " | ");
+				
+				printSpaces(7 - String.valueOf(resultSet.getInt(2)).length());
+				System.out.println(resultSet.getInt(2));
+			}
+			System.out.println();
+			
+		} catch (ClassNotFoundException e) {
+	        System.out.println("Error: JDBC driver class '" + driver + "' not found.");
+	        e.printStackTrace();
+		} catch (SQLException e) {
+	        System.out.println("SQL Error: " + e.getMessage());
+		} finally {
+			try {
+	            if (resultSet != null) {
+	            		resultSet.close();
+	            }
+	            if (statement != null) {
+	            	statement.close();
+	            }
+	            if (connection != null) {
+	            	connection.close();
+	            }
+	        } catch (SQLException e) {
+	            System.out.println("Error closing database resources: " + e.getMessage());
+	        }
+		}	
+	}
+	
+	public void printProducerTable() {
+		try {
+			Class.forName(driver);
+			
+			connection = DriverManager.getConnection(url, user, password);
+			
+			statement = connection.createStatement();
+			
+			resultSet = statement.executeQuery(
+					"SELECT * FROM PRODUCER");
+			
+			System.out.println("PRODUCER\n");
+			System.out.println("ProducerID |         Name         ");
+			System.out.println("----------------------------------");
+			while (resultSet.next()) {
+				printSpaces(10 - String.valueOf(resultSet.getInt(1)).length());
+				System.out.print(resultSet.getInt(1) + " | ");
+				
+				printSpaces(21 - resultSet.getString(2).length());
+				System.out.println(resultSet.getString(2));
+			}
+			System.out.println();
+			
+		} catch (ClassNotFoundException e) {
+	        System.out.println("Error: JDBC driver class '" + driver + "' not found.");
+	        e.printStackTrace();
+		} catch (SQLException e) {
+	        System.out.println("SQL Error: " + e.getMessage());
+		} finally {
+			try {
+	            if (resultSet != null) {
+	            		resultSet.close();
+	            }
+	            if (statement != null) {
+	            	statement.close();
+	            }
+	            if (connection != null) {
+	            	connection.close();
+	            }
+	        } catch (SQLException e) {
+	            System.out.println("Error closing database resources: " + e.getMessage());
+	        }
+		}
+	}
+
+	public void printRecordLabelTable() {
+		try {
+			Class.forName(driver);
+			
+			connection = DriverManager.getConnection(url, user, password);
+			
+			statement = connection.createStatement();
+			
+			resultSet = statement.executeQuery(
+					"SELECT * FROM RECORD_LABEL");
+			
+			System.out.println("RECORD_LABEL\n");
+			System.out.println("LabelID |         Name         |          Adress          |    PhoneNumber");
+			System.out.println("--------------------------------------------------------------------------");
+			while (resultSet.next()) {
+				printSpaces(7 - String.valueOf(resultSet.getInt(1)).length());
+				System.out.print(resultSet.getInt(1) + " | ");
+				
+				printSpaces(20 - resultSet.getString(2).length());
+				System.out.print(resultSet.getString(2) + " | ");
+				
+				printSpaces(24 - resultSet.getString(3).length());
+				System.out.print(resultSet.getString(3) + " | ");
+				
+				printSpaces(14 - resultSet.getString(4).length());
+				System.out.println(resultSet.getString(4));
+			}
+			System.out.println();
+			
+		} catch (ClassNotFoundException e) {
+	        System.out.println("Error: JDBC driver class '" + driver + "' not found.");
+	        e.printStackTrace();
+		} catch (SQLException e) {
+	        System.out.println("SQL Error: " + e.getMessage());
+		} finally {
+			try {
+	            if (resultSet != null) {
+	            		resultSet.close();
+	            }
+	            if (statement != null) {
+	            	statement.close();
+	            }
+	            if (connection != null) {
+	            	connection.close();
+	            }
+	        } catch (SQLException e) {
+	            System.out.println("Error closing database resources: " + e.getMessage());
+	        }
+		}
+	}
+	
+	public void printSongTable() {
+		try {
+			Class.forName(driver);
+			
+			connection = DriverManager.getConnection(url, user, password);
+			
+			statement = connection.createStatement();
+			
+			resultSet = statement.executeQuery(
+					"SELECT * FROM SONG");
+			
+			System.out.println("SONG\n");
+			System.out.println("SongID |         Name         | TrackLength | AlbumID | GenreID | ProducerID");
+			System.out.println("----------------------------------------------------------------------------");
+			while (resultSet.next()) {
+				printSpaces(6 - String.valueOf(resultSet.getInt(1)).length());
+				System.out.print(resultSet.getInt(1) + " | ");
+				
+				printSpaces(20 - resultSet.getString(2).length());
+				System.out.print(resultSet.getString(2) + " | ");
+				
+				printSpaces(11 - (resultSet.getTime(3).toString().length()));
+				System.out.print(resultSet.getTime(3) + " | ");
+				
+				printSpaces(7 - String.valueOf(resultSet.getInt(4)).length());
+				System.out.print(resultSet.getInt(4) + " | ");
+				
+				printSpaces(7 - String.valueOf(resultSet.getInt(5)).length());
+				System.out.print(resultSet.getInt(5) + " | ");
+				
+				printSpaces(10 - String.valueOf(resultSet.getInt(6)).length());
+				System.out.println(resultSet.getInt(6));
+			}
+			System.out.println();
+			
+		} catch (ClassNotFoundException e) {
+	        System.out.println("Error: JDBC driver class '" + driver + "' not found.");
+	        e.printStackTrace();
+		} catch (SQLException e) {
+	        System.out.println("SQL Error: " + e.getMessage());
+		} finally {
+			try {
+	            if (resultSet != null) {
+	            		resultSet.close();
+	            }
+	            if (statement != null) {
+	            	statement.close();
+	            }
+	            if (connection != null) {
+	            	connection.close();
+	            }
+	        } catch (SQLException e) {
+	            System.out.println("Error closing database resources: " + e.getMessage());
+	        }
+		}
+	}
+	
+	public void printWorksForTable() {
+		try {
+			Class.forName(driver);
+			
+			connection = DriverManager.getConnection(url, user, password);
+			
+			statement = connection.createStatement();
+			
+			resultSet = statement.executeQuery(
+					"SELECT * FROM WORKS_FOR");
+			
+			System.out.println("WORKS_FOR\n");
+			System.out.println("ProducerID | LabelID");
+			System.out.println("--------------------");
+			while (resultSet.next()) {
+				printSpaces(10 - String.valueOf(resultSet.getInt(1)).length());
+				System.out.print(resultSet.getInt(1) + " | ");
+				
+				printSpaces(7 - String.valueOf(resultSet.getInt(2)).length());
+				System.out.println(resultSet.getInt(2));
+			}
+			System.out.println();
+			
+		} catch (ClassNotFoundException e) {
+	        System.out.println("Error: JDBC driver class '" + driver + "' not found.");
+	        e.printStackTrace();
+		} catch (SQLException e) {
+	        System.out.println("SQL Error: " + e.getMessage());
+		} finally {
+			try {
+	            if (resultSet != null) {
+	            		resultSet.close();
+	            }
+	            if (statement != null) {
+	            	statement.close();
+	            }
+	            if (connection != null) {
+	            	connection.close();
+	            }
+	        } catch (SQLException e) {
+	            System.out.println("Error closing database resources: " + e.getMessage());
+	        }
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	public void printSpaces(int count) {
+		String spaces = "";
+		for (int i = 0; i < count ; i++) {
+			spaces += " ";
+		}
+		System.out.print(spaces);
+	}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,108 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	private static DatabaseConnect db;
+	private static Scanner scanner;
+	
+	private static char choice;
+	
+	/**Entry point into application*/
+	public static void main(String[] args) {
+		if (args.length != 4) {
+			System.out.println("Wrong number of arguments provided. Usage:\n");
+			System.out.println("java src.Main <url> <user> <password> <driver>");
+			System.exit(1);
+		}
+		
+		System.out.println("Welcome to Simple Music Storage (SMS)!\n");
+		
+		//Connect to database through DatabaseConnect class
+		db = new DatabaseConnect(args);
+		
+		scanner = new Scanner(System.in);
+		
+		do {
+			printMenu();
+			choice = scanner.next().toLowerCase().charAt(0);
+			System.out.println();
+			
+			switch (choice) {
+				case '1':
+					chooseTablePrint();
+					break;
+			
+			
+				case 'q':
+					break;
+				default:
+					System.out.println("Not a valid choice\n");
+			}
+			
+		} while (choice != 'q');
+		
+		scanner.close();
+		System.out.println("Exiting application...");
+	}
+	
+	public static void printMenu() {
+		System.out.println("Enter menu choice:");
+		System.out.println("\t1: print a table in database");
+		
+		System.out.println("\tq: quit");
+	}
+	
+	public static void chooseTablePrint() {
+		do {
+			System.out.println("Choose a table to print:");
+			System.out.println("\t1: ALBUM");
+			System.out.println("\t2: ARTIST");
+			System.out.println("\t3: CREATED");
+			System.out.println("\t4: GENRE");
+			System.out.println("\t5: MADE");
+			System.out.println("\t6: PRODUCER");
+			System.out.println("\t7: RECORD_LABEL");
+			System.out.println("\t8: SONG");
+			System.out.println("\t9: WORKS_FOR");
+			System.out.println("\te: exit selection");
+			
+			choice = scanner.next().toLowerCase().charAt(0);
+			System.out.println();
+			
+			switch (choice) {
+				case '1':
+					db.printAlbumTable();
+					break;
+				case '2':
+					db.printArtistTable();
+					break;
+				case '3':
+					db.printCreatedTable();
+					break;
+				case '4':
+					db.printGenreTable();
+					break;
+				case '5':
+					db.printMadeTable();
+					break;
+				case '6':
+					db.printProducerTable();
+					break;
+				case '7':
+					db.printRecordLabelTable();
+					break;
+				case '8':
+					db.printSongTable();
+					break;
+				case '9':
+					db.printWorksForTable();
+					break;
+				case 'e':
+					break;
+				default:
+					System.out.println("Not a valid choice. Try aqain.");
+					
+			}
+		} while (choice != 'e');
+	}
+}
